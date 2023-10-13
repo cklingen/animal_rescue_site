a65133541235914a8c91a7e8f65ba9d3a581b136
Cindy Klingensmith <clanyon06@yahoo.com>
2023-10-13 10:58:19 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo style={{ height: 300}}/>
+          <h1>Hello</h1>
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <div className='App-header'>Animal Rescue</div>;
+};
+
+export default Header;

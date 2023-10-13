5afcb5066f85a7108b6c68c27a719dc90a470aa7
Cindy Klingensmith <clanyon06@yahoo.com>
2023-10-13 13:54:08 -0400

    logo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { ReactComponent as Logo } from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import a_logo from './app/assets/img/a_logo.jpg';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar color='black' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={a_logo} alt='rescue logo'/>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
       <header className="App-header">
-        <Logo style={{ height: 300}}/>
-          <h1>Hello</h1>
+        <Logo style={{ height: 300 }} />
+        <h1>Hello</h1>
       </header>
     </div>
   );
